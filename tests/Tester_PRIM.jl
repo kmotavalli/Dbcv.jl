72aba07e04e395e8b98c5694f913752cd7e65c73
kmotavalli <keivan@motavalli.me>
2026-06-24 19:30:31 +0200

    Julia tester main/wrapper: add option supporting loading csv datasets with a header

--- tests/Tester_PRIM.jl
+++ tests/Tester_PRIM.jl
@@ -1,10 +1,19 @@
 push!(LOAD_PATH, "../src")
 import Dbcv, DelimitedFiles
 
+has_header::Integer = 0
+dataset::AbstractArray = []
 dataset_file::String, clustering_file::String = ARGS
 
-dataset::AbstractArray = DelimitedFiles.readdlm(dataset_file, ',', BigFloat)
+if size(ARGS, 1) == 3
+    has_headers = ARGS[3]
+end
 
+if has_header != nothing && has_header == 1
+    dataset = DelimitedFiles.readdlm(dataset_file, ',', BigFloat, skipstart=1)
+else
+    dataset = DelimitedFiles.readdlm(dataset_file, ',', BigFloat)
+end
 clustering::AbstractArray = DelimitedFiles.readdlm(clustering_file, ',', Int)
 
 result::Real = Dbcv.dbcv(dataset, vec(clustering[:, 1]))
